9f9adef8a905bdb4794c940db00539358668eb84
robfrank <ro.franchini@gmail.com>
2019-06-21 08:25:09 +0200

    reviews orientdb 2.2.x image build script
    bumps orientdb to 2.2.37

--- docker/orientdb/build.sh
+++ docker/orientdb/build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-docker build . -t arcadeanalytics/orientdb:2.2.36 --pull
+mkdir ./demodb
 
-docker push arcadeanalytics/orientdb:2.2.36
+wget -O ./demodb.zip  https://orientdb.com/public-databases/demodb.zip
+
+unzip -d ./demodb/ ./demodb.zip
+
+rm ./demodb.zip
+
+docker build . -t  arcadeanalytics/orientdb:2.2.37 --pull --no-cache
+
+rm -rf ./demodb/
+
+docker push arcadeanalytics/orientdb:2.2.37
+
